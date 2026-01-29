12a578c64ef3049f7e5deee11431247e0a59c427
SmartMonkey <rouven.reuter91@gmail.com>
2026-01-29 21:35:24 +0100

    impl AsRef for curie

--- src/curie.rs
+++ src/curie.rs
@@ -2,28 +2,34 @@ use std::fmt::{Display, Formatter};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Curie {
-    prefix: String,
-    reference: String,
+    inner: String,
+    prefix_len: usize,
 }
 
 impl Curie {
     pub(crate) fn new(prefix: &str, reference: &str) -> Curie {
-        Curie {
-            prefix: prefix.to_string(),
-            reference: reference.to_string(),
-        }
+        let inner = format!("{prefix}:{reference}");
+        let prefix_len = prefix.len();
+        Curie { inner, prefix_len }
     }
 
     pub fn prefix(&self) -> &str {
-        &self.prefix
+        &self.inner[..self.prefix_len]
     }
+
     pub fn reference(&self) -> &str {
-        &self.reference
+        &self.inner[self.prefix_len + 1..]
     }
 }
 
 impl Display for Curie {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&format!("{}:{}", self.prefix, self.reference))
+        f.write_str(&self.inner)
+    }
+}
+
+impl AsRef<str> for Curie {
+    fn as_ref(&self) -> &str {
+        &self.inner
     }
 }
